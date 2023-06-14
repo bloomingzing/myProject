22d62d65c848eedf011622fc811e088283f34538
bloomingzing <46523065+bloomingzing@users.noreply.github.com>
2023-06-14 02:17:22 -0700

    Newest update on remote

--- src/println.java
+++ src/println.java
@@ -2,6 +2,6 @@ public class println {
     public static void main(String[] args) {
         System.out.println("Hello world! Let's go! Try newBranch! I am from remote!");
         System.out.println("I am confict with stash!");
-        System.out.println("I am new update, but local use my old version to develop");
+        System.out.println("I am a new update, but local use my old version to develop");
     }
 }
